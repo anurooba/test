9712c84321d8026aeecf46de22a01f69d6768d79
anurooba <anurooba@ucsb.edu>
2017-04-26 22:18:44 -0700

    R changes and markdown creation

--- sun.simpleexample.R
+++ sun.simpleexample.R
@@ -5,3 +5,8 @@ boxplot(sunlight$Kdown_direct~sunlight$month,
   ylab="Downwelling Solar\n kj/m2/day",
   xlab="month", col="orange")
 
+print("Added the sunshine - this is a change")
+
+print("If it is sunny, it ought to shine")
+
+print("Making a local change to check if the colored arrows push and pull the file to the Github repo")
